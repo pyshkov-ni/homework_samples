4397733bc1224cc36a0c13a75e041863aeae63b4
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-09 17:55:24 +0300

    songs.py - добавил решение с методом split() и индексацией списков

--- 02_data_types/02_string/songs.py
+++ 02_data_types/02_string/songs.py
@@ -3,3 +3,8 @@ my_favorite_songs = 'Waste a Moment, New Salvation, Staying\' Alive, Out of Touc
 
 # Выведите на консоль с помощью индексации, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
