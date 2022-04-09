158109ba318edff07cb4a7270c53fb6c56e062ed
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-09 17:53:22 +0300

    a. 02_data_types/02_string/songs.py добавлен b. songs.py - Добавил описание задачи

---
+++ 02_data_types/02_string/songs.py
@@ -0,0 +1,5 @@
+# Есть строка с перечислением песен
+my_favorite_songs = 'Waste a Moment, New Salvation, Staying\' Alive, Out of Touch, A Sorta Fairytale'
+
+# Выведите на консоль с помощью индексации, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
