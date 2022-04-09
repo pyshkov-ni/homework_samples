34b49121bdcc8eb2bd1dc6e744a35aa44e3afebf
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-09 17:56:51 +0300

    songs.py - добавил решение с индексацией str и поиском индекса find()

--- 02_data_types/02_string/songs.py
+++ 02_data_types/02_string/songs.py
@@ -8,3 +8,24 @@ my_favorite_songs = 'Waste a Moment, New Salvation, Staying\' Alive, Out of Touc
 songs = my_favorite_songs.split(', ')
 
 print(songs[0], songs[-1], songs[1], songs[-2])
+
+# Решение с помощью индекции строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('A') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('N') : 
+     my_favorite_songs.find(', S')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('O') : my_favorite_songs.rfind(',')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
