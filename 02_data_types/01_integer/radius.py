455a177554625faabc0fdb4cf90327664e47ead2
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-09 17:49:43 +0300

    radius.py - написал код по условию задачи

--- 02_data_types/01_integer/radius.py
+++ 02_data_types/01_integer/radius.py
@@ -6,3 +6,11 @@
 # число пи pi = 3.1415926 
 # Выведите ответ с точностью до 4-х знаков после запятой
 # Точность указывается в функции round()
+
+radius = 16
+pi = 3.1415926
+
+square_cycle = pi * radius ** 2
+square_cycle_round = round(square_cycle, 4)
+
+print(square_cycle_round)
