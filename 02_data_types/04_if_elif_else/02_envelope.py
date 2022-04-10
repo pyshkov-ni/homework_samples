ff6bb1c44b9a1d560437bf76e5bd21d78acb30b2
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-10 21:51:31 +0300

    02_envelope - условие и перемнные envelop_x/y и paper_x/y изменены на box_x/y и product_x/y

--- 02_data_types/04_if_elif_else/02_envelope.py
+++ 02_data_types/04_if_elif_else/02_envelope.py
@@ -1,40 +1,40 @@
-# Заданы размеры envelop_x, envelop_y - размеры конверта и paper_x, paper_y листа бумаги
-# Определить, поместится ли бумага в конверте (стороны листа параллельны сторонам конверта)
+# Заданы размеры коробки box_x, box_y и товара product_x, product_y
+# Определить, поместится ли товар в коробке (габариты товара параллельны размеру коробки)
 # Результат проверки вывести на консоль (ДА/НЕТ)
 # Использовать только операторы if/elif/else, можно вложенные
 
-envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+box_x, box_y = 10, 7
+product_x, product_y = 8, 9
 
 # проверить для
-# paper_x, paper_y = 9, 8
-# paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
-# paper_x, paper_y = 3, 4
-# paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
+# product_x, product_y = 9, 8
+# product_x, product_y = 6, 8
+# product_x, product_y = 8, 6
+# product_x, product_y = 3, 4
+# product_x, product_y = 11, 9
+# product_x, product_y = 9, 11
 
 # Раскомментируйте нужную строку
 
-print('*'*7, 'Поместиться ли лист в конверт?'.upper(), '*'*7)
-print('Размер конверта:', envelop_x, 'на', envelop_y)
-print('Размер листа:', paper_x, 'на', paper_y)
+print('*'*7, 'Поместиться ли товар в коробке?'.upper(), '*'*7)
+print('Размер коробки:', box_x, 'на', box_y)
+print('Размер товара:', product_x, 'на', product_y)
 
 # Решение 1
-# if paper_x <= envelop_x:
-#     if paper_y <= envelop_y:
-#         print('ДА!')
-#     else: 
-#         print('НЕТ!')
-# else:
-#     print('НЕТ!')
+if product_x <= box_x:
+    if product_y <= box_y:
+        print('ДА!')
+    else: 
+        print('НЕТ!')
+else:
+    print('НЕТ!')
 
 # Решение 2
-if paper_x <= 0 or paper_y <= 0:
+if product_x <= 0 or product_y <= 0:
     print('Размер листа не может быть меньше 0!')
-elif paper_x > envelop_x:
+elif product_x > box_x:
     print('НЕТ!')
-elif paper_y > envelop_y:
+elif product_y > box_y:
     print('НЕТ!')
 else:
     print('ДА!')
