12ba0559252cc14eb0414ae5e4e8a7c8294c4dfa
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-10 23:10:40 +0300

    02_box - исправил ошибку

--- 02_data_types/04_if_elif_else/02_box.py
+++ 02_data_types/04_if_elif_else/02_box.py
@@ -21,35 +21,20 @@ print('Размер коробки:', box_x, 'на', box_y)
 print('Размер товара:', product_x, 'на', product_y)
 
 # Решение 1
-# if product_x <= box_x:
-#     if product_y <= box_y:
-#         print('ДА!')
-#     else: 
-#         print('НЕТ!')
-# else:
-#     print('НЕТ!')
+if product_x <= box_x:
+    if product_y <= box_y:
+        print('ДА!')
+    else: 
+        print('НЕТ!')
+else:
+    print('НЕТ!')
 
-<<<<<<< HEAD
-# # Решение 2
-# if product_x <= 0 or product_y <= 0:
-#     print('Размер товара не может быть меньше 0!')
-# elif product_x > box_x:
-#     print('НЕТ!')
-# elif product_y > box_y:
-#     print('НЕТ!')
-# else:
-#     print('ДА!')
-
-# Решение 3
-if product_x > box_x or product_y > box_y:
-=======
 # Решение 2
 if product_x <= 0 or product_y <= 0:
     print('Размер товара не может быть меньше 0!')
 elif product_x > box_x:
     print('НЕТ!')
 elif product_y > box_y:
->>>>>>> f3d68a9 (a. 02_box - добавил решение 3)
     print('НЕТ!')
 else:
     print('ДА!')
