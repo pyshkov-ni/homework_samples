e064ab3b8c352008809a9382023ebe12f84d9aeb
pyshkov-ni <guess_the_tutor@gmail.com>
2022-04-10 21:02:10 +0300

    01_calendar - Добавлено решение 1 через if

---
+++ 02_data_types/04_if_elif_else/01_calendar.py
@@ -0,0 +1,26 @@
+# Вывести По номеру месяца вывести кол-во дней в нем (без указания названия месяца, в феврале 28 дней)
+# Результат проверки вывести на консоль
+# Если номер месяца некорректен - сообщить об этом
+user_input = input("Введите номер месяца: ")
+month = int(user_input)
+print('Вы ввели', month)
+
+# Решение 1
+if month in [1, 3, 5, 7, 8, 10, 12]:
+    print(31)
+elif month in [4, 6, 9, 11]:
+    print(30)
+elif month == 2:
+    print(28)
+else:
+    print('Введен неправильный номер месяца!')
+
+# Решение 2
+import calendar as cl
+
+year_input = input("Введите год: ")
+month_input = input("Введите номер месяца: ")
+
+year = int(year_input)
+month_ = int(month_input)
+print(cl.monthrange(year, month_))
